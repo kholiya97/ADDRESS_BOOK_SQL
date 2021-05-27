d5b926399178b7ca39d06d276812cf93802a4044
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-05-27 21:07:40 +0530

    initial Commit

--- Addressbook.sql
+++ Addressbook.sql
@@ -50,3 +50,34 @@ Select COUNT(StateName) as State_Count FROM Address_Book;
 ----------------UC8--------------
 -----------Sorting data------------
 SELECT * FROM Address_Book ORDER BY(FirstName);
+
+---UC9:- Ability to identify each Address Book with name and Type.
+
+--UC9.1:- Alter Address Book to add name and type.
+alter table Address_Book add Name varchar(255),Type varchar(255);
+
+select * from Address_Book;
+
+--UC9.2:- Here the type could Family, Friends, Profession, etc.
+
+update Address_Book set Name='JAISHREERAM';
+
+update Address_Book set Type ='Family' where FirstName='Vishal';
+select * from Address_Book;
+
+update Address_Book set Type ='Friend' where FirstName='Himanshu'; 
+select * from Address_Book;
+
+--UC10:- Ability to get number of contact persons i.e. count by type.
+
+select count(type) 
+from Address_Book;
+
+select Type, count(*) as TypeCount 
+from Address_Book
+group by Type;
+
+--UC11:- Ability to add person to both Friend and Family.
+
+insert into Address_Book(FirstName,LastName,City,StateName,Zip,Phone,Email,Name,Type) values('Kumar','Mumbai','Maharashtra','400105','8762498989','sham@gmail.com','ShamContact','Family');
+select * from Address_Book;
